993c5f3e052857177833b021c9c39a7767e0ebe0
Catarina11 <202000594@estudantes.ips.pt>
2024-01-09 17:13:27 +0000

    commit

---
+++ PROJETOESA/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PROJETOESA.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
