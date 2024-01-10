61f52026b3dd22d9f2995b917132ae8a819ef1fa
AntonioRRips <72794431+AntonioRRips@users.noreply.github.com>
2024-01-10 19:00:38 +0000

    Utilizador começado

---
+++ PROJETOESA/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.Security.Permissions;
+
+namespace PROJETOESA.Models
+{
+    public class User : IdentityUser
+    {
+        [Key]
+        public Guid UserId { get; set; }
+    }
+}
