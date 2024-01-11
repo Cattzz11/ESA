561eee38c4e825c2afdf670d78a701efaf249f8c
TiagoRodrigues201400314 <201400314@estudantes.ips.pt>
2024-01-11 23:45:05 +0000

    Passei o menu e pagina home para o lado do angular

--- AngularESA/angularesa.client/src/app/app.module.ts
+++ AngularESA/angularesa.client/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    HomeComponent,
+    NavMenuComponent
   ],
   imports: [
     BrowserModule, HttpClientModule,
