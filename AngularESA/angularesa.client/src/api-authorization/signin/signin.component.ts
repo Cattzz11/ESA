ff8222d5a2e0a9271a1263f1c70b6597ad2e2d34
Catarina11 <202000594@estudantes.ips.pt>
2024-02-07 13:04:05 +0000

    updates

--- AngularESA/angularesa.client/src/api-authorization/signin/signin.component.ts
+++ AngularESA/angularesa.client/src/api-authorization/signin/signin.component.ts
@@ -12,6 +12,7 @@ export class SignInComponent implements OnInit {
   loginForm!: FormGroup;
   authFailed: boolean = false;
   signedIn: boolean = false;
+  isLoggedIn: boolean = false;
 
   constructor(private authService: AuthorizeService,
     private formBuilder: FormBuilder,
@@ -29,7 +30,40 @@ export class SignInComponent implements OnInit {
         email: ['', [Validators.required, Validators.email]],
         password: ['', [Validators.required]]
       });
+
+    this.initializeGoogleOnTap();
+
+
   }
+
+  initializeGoogleOnTap() {
+    (window as any).onGoogleLibraryLoad = () => {
+      console.log('Google\'s One-tap sign in script loaded!');
+
+      //@ts-ignore
+      google.accounts.id.initialize({
+        // Ref: https://developers.google.com/identity/gsi/web/reference/js-reference#IdConfiguration
+        client_id: '712855861147-lt433p2k7stok4g5h6hvba6qmt7iktld.apps.googleusercontent.com',
+        callback: this.googleResponse.bind(this),
+        auto_select: true,
+        cancel_on_tap_outside: false
+      });
+
+      //@ts-ignore
+      google.accounts!.id.renderButton(document!.getElementById('login-googleBTN')!, { theme: 'outline', size: 'large', width: 200 })
+      //@ts-ignore
+      google.accounts.id.prompt();
+    };
+
+  }
+
+  async googleResponse(response: any) {
+    if (response && response.credential)
+      this.isLoggedIn = true;
+
+    console.log('RESPONSE :>> ', response);
+  }
+
 
   public signIn(_: any) {
     if (!this.loginForm.valid) {
