720c65e644556085a24f6242d1a549c972d41d8f
Daniel Roque <73941200+iSkaly@users.noreply.github.com>
2024-03-08 22:50:36 +0000

    for deletion

--- PROJETOESA/Models/Calendar.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PROJETOESA.Models
-{
-    public class Calendar
-    {
-        public DateTime Date {  get; set; }
-        public string Category { get; set; }
-        public double Price { get; set; }
-    }
-}
