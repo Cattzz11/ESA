128c6b5037cb94e6006834b4947f62357c5e6ca6
Daniel Roque <73941200+iSkaly@users.noreply.github.com>
2024-03-11 23:42:17 +0000

    done

--- PROJETOESA/obj/Debug/net8.0/PROJETOESA.AssemblyInfo.cs
+++ PROJETOESA/obj/Debug/net8.0/PROJETOESA.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PROJETOESA")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+187bc77ee37eae496b0bcc5214a495f1f407fbd8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6b69c7f8bd2c533b1137d48eb0fa5889026c14f")]
 [assembly: System.Reflection.AssemblyProductAttribute("PROJETOESA")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PROJETOESA")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
