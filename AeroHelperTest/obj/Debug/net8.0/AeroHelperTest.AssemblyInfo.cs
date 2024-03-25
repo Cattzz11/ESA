cdadfb746af53d54d91e90fde2a2a5632a39901e
Daniel Roque <73941200+iSkaly@users.noreply.github.com>
2024-03-25 22:41:02 +0000

    Merge Estatisticas -> Dev

--- AeroHelperTest/obj/Debug/net8.0/AeroHelperTest.AssemblyInfo.cs
+++ AeroHelperTest/obj/Debug/net8.0/AeroHelperTest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AeroHelperTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+784953ffc63d4f30ac07e6c1c85da8aa5f3265f7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+061358923ce4987b0f7f9b469245ec3dd079a408")]
 [assembly: System.Reflection.AssemblyProductAttribute("AeroHelperTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AeroHelperTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
